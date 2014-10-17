da68c1582d88f923d668ed213eba0d02ae013fec
kshimatsu <kristenshimatsu@gmail.com>
2014-10-17 15:30:39 +0800

    Added random sample of dishes, need to change to loop

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,13 @@ class WelcomeController < ApplicationController
 
   def index
     @delivery_time = Time.now + 45.minutes
+
+    # @item = FoodItem.find_by_name("Crab Yellow Curry")
+    @item1 = FoodItem.where(cuisine: "Western").sample
+    @item2 = FoodItem.where(cuisine: "Asian").sample
+    @item3 = FoodItem.where(cuisine: "Adventurous").sample
+    @item4 = FoodItem.where(cuisine: "Western").sample
+
     render 'index'
   end
 
