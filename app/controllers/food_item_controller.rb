846933b7dfc651db1d43b498bae6212e25b5ff89
kshimatsu <kristenshimatsu@gmail.com>
2014-10-17 18:20:27 +0800

    added link to the food photos

---
+++ app/controllers/food_item_controller.rb
@@ -0,0 +1,6 @@
+class FoodItemController < ApplicationController
+  def show
+    @item = FoodItem.find params[:id]
+
+  end
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,10 +4,11 @@ class WelcomeController < ApplicationController
     @delivery_time = Time.now + 45.minutes
 
     # @item = FoodItem.find_by_name("Crab Yellow Curry")
-    @item1 = FoodItem.where(cuisine: "Western").sample
-    @item2 = FoodItem.where(cuisine: "Asian").sample
-    @item3 = FoodItem.where(cuisine: "Adventurous").sample
-    @item4 = FoodItem.where(cuisine: "Western").sample
+    @item = FoodItem.all.sample
+    @western_item = FoodItem.where(cuisine: "Western").sample
+    @asian_item = FoodItem.where(cuisine: "Asian").sample
+    @adventurous_item = FoodItem.where(cuisine: "Adventurous").sample
+    @western_item = FoodItem.where(cuisine: "Western").sample
 
     render 'index'
   end
